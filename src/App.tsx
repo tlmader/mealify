e8d29e2dd44fd4e48bca777a18699c2f3544ff4f
Ted Mader <tlmader.dev@gmail.com>
2019-05-29 09:28:49 -0500

    Style App component

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,15 @@ const App: React.FC = () => {
   return (
     <MealContext.Provider value={defaultMeals}>
       <div className="App">
-        <MealList />
-        <MealForm />
+        <div>
+          <h1>YumLog</h1>
+          <h4>Your trusty meal logger!</h4>
+        </div>
+        <div className="App__divider" />
+        <div className="App__content">
+          <MealList />
+          <MealForm />
+        </div>
       </div>
     </MealContext.Provider>
   );

a66eb08c3cfb7e48cd6f76beb015df75d8f70c17
Ted Mader <tlmader.dev@gmail.com>
2019-05-29 11:49:33 -0500

    Create FoodItemContext

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,25 @@ import './App.css';
 import { MealList } from './components/MealList';
 import { MealProvider } from './contexts/MealContext';
 import { CreateMealView } from './components/CreateMealView';
+import { FoodItemProvider } from './contexts/FoodItemContext';
 
 const App: React.FC = () => {
   return (
-    <MealProvider>
-      <div className="App">
-        <div>
-          <h1>YumLog</h1>
-          <h4>Your trusty meal logger!</h4>
+    <FoodItemProvider>
+      <MealProvider>
+        <div className="App">
+          <div>
+            <h1>YumLog</h1>
+            <h4>Your trusty meal logger!</h4>
+          </div>
+          <div className="App__divider" />
+          <div className="App__content">
+            <MealList />
+            <CreateMealView />
+          </div>
         </div>
-        <div className="App__divider" />
-        <div className="App__content">
-          <MealList />
-          <CreateMealView />
-        </div>
-      </div>
-    </MealProvider>
+      </MealProvider>
+    </FoodItemProvider>
   );
 }
 

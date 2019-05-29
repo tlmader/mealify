d2e48339cb556f55c89bb4aa521f16ea0d1c4513
Ted Mader <tlmader.dev@gmail.com>
2019-05-28 22:43:20 -0500

    Show required errors and filter negative input

--- src/components/FoodItemInput.tsx
+++ src/components/FoodItemInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FoodItemInputValue } from '../types/FoodItemInputValue';
 import './FoodItemInput.css';
 
@@ -8,24 +8,47 @@ export interface FoodItemInputProps {
   onChange: (index: number, value: Partial<FoodItemInputValue>) => void;
 }
 
-export const FoodItemInput: React.FC<FoodItemInputProps> = ({ index, value, onChange }) => {
-  const handleChangeForEvent = (eventType: keyof FoodItemInputValue) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    onChange(index, { [eventType]: event.target.value })
+const initialState = {
+  touched: {
+    name: false,
+    calories: false,
+    portion: false,
   }
+}
+type State = Readonly<typeof initialState>;
+
+export const FoodItemInput: React.FC<FoodItemInputProps> = ({ index, value, onChange }) => {
+  const [{ touched }, setState] = useState<State>(initialState);
+
+  const handleBlur = (key: keyof FoodItemInputValue) => () => {
+    if (!touched[key]) {
+      setState(prevState => ({ touched: { ...prevState.touched, [key]: true } }));
+    }
+  }
+
+  const handleChangeForEvent = (key: keyof FoodItemInputValue) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    onChange(index, { [key]: (key === 'calories' || key === 'portion') && value ? Math.abs(+value) : value });
+  }
+
   return (
     <div className="FoodItemInput">
-      <h4>Food Item {index + 1}</h4>
+      <div className="FoodItemInput__heading">
+        <h4>Food Item {index + 1}</h4>
+        <button>Delete</button>
+      </div>
       <div>
         <label>
           Name
           <div className="FoodItemInput__input">
             <input
               value={value.name}
+              onBlur={handleBlur('name')}
               onChange={handleChangeForEvent('name')}
               type="text"
               placeholder="Enter name"
             />
+            {touched.name && !value.name && <div className="FoodItemInput__error">Name is required</div>}
           </div>
         </label>
       </div>
@@ -35,11 +58,13 @@ export const FoodItemInput: React.FC<FoodItemInputProps> = ({ index, value, onCh
           <div className="FoodItemInput__input">
             <input
               value={value.calories}
+              onBlur={handleBlur('calories')}
               onChange={handleChangeForEvent('calories')}
               type="number"
               min="0"
             />
             calories
+            {touched.calories && !value.calories && <div className="FoodItemInput__error">Calories is required</div>}
           </div>
         </label>
       </div>
@@ -49,11 +74,13 @@ export const FoodItemInput: React.FC<FoodItemInputProps> = ({ index, value, onCh
           <div className="FoodItemInput__input">
             <input
               value={value.portion}
+              onBlur={handleBlur('portion')}
               onChange={handleChangeForEvent('portion')}
               type="number"
               min="0"
             />
             grams
+            {touched.portion && !value.portion && <div className="FoodItemInput__error">Portion is required</div>}
           </div>
         </label>
       </div>

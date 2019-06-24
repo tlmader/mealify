fe28ed2128d2b5942d0c8b62d4eea78114c6e90d
Ted Mader <tlmader.dev@gmail.com>
2019-06-24 10:37:50 -0500

    Updates

---
+++ precache-manifest.ee8a3c26122c1e4d261cb50e04ba797b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89e1c457adcf5f4edc9089566c40771",
+    "url": "/mealify/index.html"
+  },
+  {
+    "revision": "d951ea5b1a4ddb4e86eb",
+    "url": "/mealify/static/css/main.b2757f96.chunk.css"
+  },
+  {
+    "revision": "77abc3b513c1762f8a17",
+    "url": "/mealify/static/js/2.87fef826.chunk.js"
+  },
+  {
+    "revision": "d951ea5b1a4ddb4e86eb",
+    "url": "/mealify/static/js/main.bb1574a4.chunk.js"
+  },
+  {
+    "revision": "086160e10c7b1bba3fae",
+    "url": "/mealify/static/js/runtime~main.bb44c324.js"
+  }
+]);
